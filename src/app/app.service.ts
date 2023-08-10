71111f299959976435d111fb9a81278099fe6478
michalharasim <mharasim74@gmail.com>
2023-08-10 15:50:26 +0200

    small change

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -3,6 +3,6 @@ import {Component, Injectable} from "@angular/core";
 @Injectable()
 export class AppService {
   getCourses() {
-    return ["course1", "course2", "course3"];
+    return ["course1", "course2", "course3, course4"];
   }
 }
